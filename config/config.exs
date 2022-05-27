7e9fd3b88cd690e55da2f0db0dff857ee429f1d1
StakNine <staknine@protonmail.com>
2022-05-26 20:15:20 -0700

    Add stripity_stripe config & add .env to .gitignore

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,8 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :stripity_stripe, api_key: System.get_env("STRIPE_API_KEY")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
